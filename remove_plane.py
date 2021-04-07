1e4f1319bafaabc5f66b45253b54e6499c911931
Harry Carstairs <s1332488@tarff.geos.ed.ac.uk>
2021-04-07 16:20:05 +0100

    first draft of plane removal algorithm

--- remove_plane.py
+++ remove_plane.py
@@ -8,43 +8,48 @@ Plane fitting to height band of processed TDX images.
 
 """
 
-import scipy.optimize as op
+from scipy.linalg import lstsq
 import numpy as np
-import rasterio as ras
-import os
+# rasterio as ras
+# import os
 
-def samples(N,array):
+
+def get_samples(N,array):
     # N is number of samples
-    # To be taken from raster in array format
+    # A is Matrix containing x,y values and B is vector of z values
     X,Y = array.shape[0], array.shape[1]
     x_vals = np.random.randint(0,X,N)
     y_vals = np.random.randint(0,Y,N)
-    z_vals = array[x_vals,y_vals]
-    return np.array([x_vals,y_vals,z_vals])
+    B = array[x_vals,y_vals]
+    A = np.ones((N,3))
+    A[:,0] = x_vals
+    A[:,1] = y_vals
+    return A, B
 
-def residual(p,S):
-    """
-    # p is list of three parameters a,b,c
-    # which describe a plane according to:
-    # ax + by + c = z
-    # This function returns the square residual
-    # obtained by subtracting a plane from samples S
-    # where S is in the format [[x_vals],[y_vals],[z_vals]]
-    """
-    left = p[0] * S[0] + p[1] * S[1] + p[2]
-    right = S[2]
-    return (np.square(left-right)).sum()
+def plane(p,x,y):
+    return p[0]*x + p[1]*y + p[2]
 
-def get_plane(array,N):
-    # Minimizes the residual to obtain 
-    # best fit plane, according to N random points
-    X = np.arange(array.shape[0])
-    Y = np.arange(array.shape[1])
-    X,Y = np.meshgrid(X,Y)
-    s = samples(N,array)
-    p0 = [0,0,s[2].mean()]
-    result = op.minimize(residual,p0,array).x
-    a,b,c = result[0],result[1],result[2]
-    plane = a*X + b*Y + c
-    return plane
+def get_coeff(array,N):
+    A,B = get_samples(N,array)
+    result = lstsq(A,B)
+    return result
+    
+def remove_plane(array,N):
+    """
+
+    Parameters
+    ----------
+    array : 2D numpy array
+    N : int, number of sample points to take
+
+    Returns
+    -------
+    residual : array with best-fit plane subtracted - same shape as input
+
+    """
+    coeff = get_coeff(array,N)[0]
+    y,x = np.meshgrid(np.arange(array.shape[1]),np.arange(array.shape[0]))
+    best_fit = plane(coeff,x,y)
+    residual = array - best_fit
+    return residual
 

6f0f64d4e9574fac3bf2990661df62f93c032c5f
Harry Carstairs <s1332488@tarff.geos.ed.ac.uk>
2021-04-27 16:43:38 +0100

    added script for SRTM topophase removal

---
+++ image_processing/remove_SRTM.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 13:54:35 2021
+
+@author: s1332488
+
+Call this script in folder containing interferograms without topophase removed
+It will simply save a copy of each interferogram with SRTM phase removed
+
+"""
+
+from snappy import ProductIO, HashMap, GPF
+import glob
+
+def topo_removal(file):
+    image = ProductIO.readProduct(file)
+    p = HashMap()
+    p.put('demName','SRTM 1Sec HGT')
+    result = GPF.createProduct('TopoPhaseRemoval',p,image)
+    # Write to temporary file
+    outfile = file.split('.dim')[0] + '_noSRTM.dim'
+    ProductIO.writeProduct(result,outfile,'BEAM-DIMAP')
+    
+files = glob.glob('TDX_IFG*.dim')
+
+for file in files:
+    print('Topographic Removal: ',file)
+    print('********')
+    topo_removal(file)
+    
+print('Done')
